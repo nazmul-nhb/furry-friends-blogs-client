b1f5f01fc7651f8044499bfeb2703fa282a9e537
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-05-13 14:43:52 +0600

    implemented tanstack table in featured blogs page

---
+++ src/components/TableFeatured/TableFeatured.jsx
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import { useReactTable, getCoreRowModel, flexRender, getSortedRowModel } from '@tanstack/react-table';
+import './TableFeatured.css';
+import { useState } from 'react';
+
+const TableFeatured = ({ data, columns }) => {
+    // const { blog_title, blogger_photo, posted_by } = blog;
+    const [sortBlog, setSortBlog] = useState([]);
+
+
+    const table = useReactTable({
+        data, columns,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        state: { sorting: sortBlog },
+        onSortingChange: setSortBlog
+    });
+
+
+    return (
+        <div className='featured-container'>
+            {/* <img src={blogger_photo} alt="" /> */}
+            <table className='featured-table'>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {
+                                headerGroup.headers.map(header => <th key={header.id} onClick={header.column.getToggleSortingHandler()}>
+                                    {flexRender(header.column.columnDef.header, header.getContext())}
+                                    {
+                                        { asc: '🔼', desc: '🔽' }[
+                                        header.column.getIsSorted() ?? null
+                                        ]
+                                    }
+                                </th>)
+                            }
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {
+                                row.getVisibleCells().map(cell => (
+                                    <td key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))
+                            }
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+TableFeatured.propTypes = {
+    columns: PropTypes.array,
+    data: PropTypes.array,
+}
+
+export default TableFeatured;
--- src/pages/FeaturedBlogs/FeaturedBlogs.jsx
+++ src/pages/FeaturedBlogs/FeaturedBlogs.jsx
@@ -2,7 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Helmet } from "react-helmet-async";
 import catLoading from '../../assets/blue-cat.svg';
-
+import TableFeatured from "../../components/TableFeatured/TableFeatured";
+import { useMemo } from 'react';
 const FeaturedBlogs = () => {
     const { isPending, isError, error, data: featuredBlogs } = useQuery({
         queryKey: ['featuredBlogs'],
@@ -11,7 +12,28 @@ const FeaturedBlogs = () => {
             return res.data;
         }
     })
+    const data = useMemo(() => featuredBlogs, [featuredBlogs]);
+    console.log(data);
+    /** @type import('@tanstack/react-table').ColumnDef<any> */
+    const columns = [
+        {
+            header: 'Serial',
+            accessorKey: '_id'
+        },
+        {
+            header: 'Blog Title',
+            accessorKey: 'blog_title'
+        },
+        {
+            header: 'Blog Owner',
+            accessorKey: 'posted_by'
+        },
+        {
+            header: 'Profile Picture',
+            accessorKey: 'blogger_photo'
+        },
 
+    ]
     console.log(featuredBlogs);
 
     if (isPending) {
@@ -36,6 +58,12 @@ const FeaturedBlogs = () => {
                 <title>Featured Blogs - Furry Friends</title>
             </Helmet>
             Featured Blogs
+
+
+            <TableFeatured data={data} columns={columns}
+            ></TableFeatured>
+
+
         </section>
     );
 };
