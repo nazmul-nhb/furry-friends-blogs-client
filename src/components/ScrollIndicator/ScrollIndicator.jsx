067e2dc8587b7aa8cc933783fd2ae1b912b2f614
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-05-31 22:49:19 +0600

    fixed scroll indicator height and gap from navbar when user is not logged in

--- src/components/ScrollIndicator/ScrollIndicator.jsx
+++ src/components/ScrollIndicator/ScrollIndicator.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const ScrollIndicator = () => {
     const [visible, setVisible] = useState(false);
-
+    const { user } = useAuth();
 
     useEffect(() => {
         const handleScrollIndicator = () => {
@@ -13,11 +14,11 @@ const ScrollIndicator = () => {
 
             // show scroll percentage beside scrollbar
             if (scrolled === 0) {
-                document.getElementById("scroll-percentage").textContent = "";
+                // document.getElementById("scroll-percentage").textContent = "";
                 setVisible(false);
             } else {
                 setVisible(true);
-                document.getElementById("scroll-percentage").textContent = ""; // Math.round(scrolled) + "%";
+                // document.getElementById("scroll-percentage").textContent = ""; // Math.round(scrolled) + "%";
             }
         };
 
@@ -26,8 +27,8 @@ const ScrollIndicator = () => {
     }, []);
 
     return (
-        <div className={`max-w-[1920px] w-full mt-[1px] flex items-center gap-1 h-3 fixed min-[430px]:top-20 top-[68px] md:top-[88px] left-0 right-0 mx-auto z-20 ${visible ? 'bg-gradient-to-l from-[#e3e7f4d4] to-[#7690e5d2]' : 'bg-transparent'}`}>
-            <div className="h-2 bg-furry w-[0%]" id="scroll-bar"></div>
+        <div className={`max-w-[1920px] w-full mt-[1px] flex items-center gap-1 h-1 md:h-2 fixed ${user ? 'min-[430px]:top-20 top-[68px] md:top-[88px]' : 'min-[430px]:top-20 max-[429px]:top-[68px] md:top-[80px] xl:top-[72px]'} left-0 right-0 mx-auto z-20 ${visible ? 'bg-gradient-to-l from-[#e3e7f4d4] to-[#7690e5d2]' : 'bg-transparent'}`}>
+            <div className="h-1 md:h-2 bg-furry w-[0%]" id="scroll-bar"></div>
             <div className="text-xs font-semibold font-kreonSerif text-furry" id="scroll-percentage"></div>
         </div>
     );
