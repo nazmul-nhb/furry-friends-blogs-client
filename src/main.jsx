c09490ea387f9ec2fdc1182620da81444ed18d43
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-26 12:17:57 +0600

    cleanup(deps): removed unnecessary deps and configs

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,6 @@ import { router } from './routes/Routes';
 
 import './index.css';
 
-import 'react-loading-skeleton/dist/skeleton.css';
-
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
